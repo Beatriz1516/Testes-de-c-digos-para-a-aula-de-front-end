de51c151e87d3097784c2bd2ebb1052c8fc974c5
Beatriz1516 <gomes.pessoa@escola.pr.gov.br>
2026-06-29 09:49:51 -0300

    -Adicionando funções de revisão.

--- Pessoas.py
+++ Pessoas.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 dados = {
-    "Nome": ["Ana, Maria", "João", "Pedro", "Lucas", None],
+    "Nome": ["Ana", "Maria", "João", "Pedro", "Lucas"],
     "Cidade": ["Londrina", "Maringá", "Londrina", None, "Cascavel"],
     "Idade": [18, 19, 18, 20, 19],
     "Nota": [8.5, 7.0, 9.0, 6.5, 8.0]
@@ -18,7 +18,19 @@ mauricio = {
 }
 
 df = pd.DataFrame(dados)
+print("======= DATAFRAME =======")
 print(df)
+
+#estatísticas
+print("\n=== ESTATÍSTICAS ===")
+print("Média de notas: ", df['Nota'].mean())
+print("Maior nota: ", df['Nota'].max())
+print("Menor nota: ", df['Nota'].min())
+print("Soma das notas: ", df['Nota'].sum())
+
+#todos os cálculos base
+print("\n=== DESCRIBE ===")
+print(df.describe())
 
 #nomes das pessoas
 pessoas = df['Nome']
